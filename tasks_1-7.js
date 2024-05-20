185c7340596ae5d9794ad6c52eded8b9d2a5de55
stboldyrev <st.boldyrev@gmail.com>
2024-05-20 08:15:36 +0100

    Added tasks_1-7.js

---
+++ tasks_1-7.js
@@ -0,0 +1,38 @@
+// #1
+// 'abcde' Получите массив букв этой строки.
+
+// const arrLetter = (str) => {
+//   let arr = [];
+//   for (let i = 0; i < str.length; i++) {
+//     arr.push(str[i]);
+//   }
+//   return arr;
+// };
+
+const arrLetter = (str) => [...str];
+
+// console.log(arrLetter("abcde"));
+
+// #2
+// 12345 Получите массив цифр этого числа.
+
+const arrNumber = (num) => [...num.toString()];
+
+// console.log(arrNumber(12345));
+
+// #3
+// 12345 Переверните его: 54321
+
+const arrNumberReverse = (num) => [...num.toString()].reverse();
+
+// console.log(arrNumberReverse(12345));
+
+// #4
+// 12345 Найдите сумму цифр этого числа.
+
+const sumArrNumbers = (num) => {
+  let sumArr = [...num.toString()];
+  return sumArr.reduce((sum, num) => (sum += +num), 0);
+};
+
+console.log(sumArrNumbers(12345));
